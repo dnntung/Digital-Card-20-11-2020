0bb9400a614f0ba0dfbf6750b2c5fbb02062018c
Terabite <518h0584@student.tdtu.edu.vn>
2020-10-31 08:49:00 +0700

    Comment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+/*PREVENT PRE-ANIMATING OF ELEMENTS */
 window.onload=document.body.classList.remove("preload");
 
+/*CARD DISPLAY*/
 function card_toggle(){
     cardCover=card.getElementsByClassName("card-cover")[0];
     cardContent=card.getElementsByClassName("content")[0];
@@ -7,79 +9,87 @@ function card_toggle(){
 
     if (card.style.zIndex!="100"){
         shadow.style.zIndex="99";
+        shadow.style.height="calc("+(600*2)+"px + "+card.style.top+")";
+        shadow.style.opacity="100%";
+
         card.style.zIndex="100";
         card.style.height="600px";
         card.style.transformOrigin="top";
         card.style.top="20%";
         card.style.left="50%";
         card.style.transform="translate(-50%, -50%) scale(2)";
-        card.style.boxShadow="0px 0px 10px 0px rgba(0,0,0, 0.5)";
-        shadow.style.height="calc("+(600*2)+"px + "+card.style.top+")";
-        shadow.style.opacity="100%";
+        
         cardCover.style.transform="rotatex(180deg)";
+
+        //Enable scrolling
         document.body.style.overflow="auto";
     }
     else{ 
-        card.style.boxShadow="";
-        card.style.top="";
-        card.style.left="";
-        card.style.height="300px";
-        card.style.transform="";
-        card.style.transformOrigin="";
         shadow.style.opacity="";
         shadow.style.backgroundColor="";
         shadow.style.position="";
         shadow.style.height="";
-        cardCover.style.transform="";
         shadow.style.zIndex="";
+
+        card.style.height="300px";
+        card.style.top="";
+        card.style.left="";
+        card.style.transform="";
+        card.style.transformOrigin="";
+
+        cardCover.style.transform="";
+
         setTimeout(function(){
             card.style.zIndex="";
         },500);
+        
+        //Disable scrolling
         document.body.style.overflow="hidden";
         
     }
+    /* TOGGLE OBJECT's SHADOW*/
+    card.classList.toggle("low-shadow");
 }
+/*NOTE DISPLAY*/
 function note_toggle(){ 
     noteContent=note.getElementsByClassName("content")[0];
     note.classList.toggle("hover");
 
     if (note.style.zIndex!="100"){
-        note.style.zIndex="100";
         shadow.style.zIndex="99";
+        shadow.style.opacity="100%";
+
+        note.style.zIndex="100";
         note.style.transformOrigin="center";
         note.style.top="50%";
         note.style.left="50%";
         note.style.transform="translate(-50%, -50%) scale(3)";
-        shadow.style.backgroundColor="rgba(0,0,0,0.75)";
-        noteContent.style.transform="rotatex(180deg)";
-        setTimeout(function(){
-            note.style.boxShadow="0px 0px 10px 0px rgba(0,0,0, 0.5)";
-        },300);
-        
+
+        noteContent.style.transform="rotatex(180deg)";        
     }
-    else{ 
-        note.style.boxShadow="";    
+    else{   
         note.style.top="";
         note.style.left="";
         note.style.transform="";
         noteContent.style.transform="";
         note.style.transformOrigin="";
-        shadow.style.backgroundColor="";
-        shadow.style.zIndex="";
+        
         setTimeout(function(){
             note.style.zIndex="";
         },300);
+        
+        shadow.style.zIndex="";
+        shadow.style.opacity="";
     }
+    /* TOGGLE OBJECT's SHADOW*/
+    note.classList.toggle("low-shadow");
 }
 /*MUSIC PLAYER*/
 var playingSong="audio/first_date.mp3";
-
 var barFill = document.getElementById("fill");
-
 var song = new Audio();
 
 window.onload = playSong;
-
 
 function playSong(){
     song.src = playingSong;
